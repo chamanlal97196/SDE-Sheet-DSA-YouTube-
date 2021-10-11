641411618d3cac9a24d35234fc353cd7f712cef3
chamanlal97196 <chamanlal97196@gmail.com>
2021-10-11 10:21:54 +0530

    Create Sort0s1s2s.cpp

---
+++ SDE-SHEET Striver/Day1/Sort0s1s2s.cpp
@@ -0,0 +1,47 @@
+/*
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+
+Input: nums = [0]
+Output: [0]
+*/
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int start=0;
+        int end=nums.size()-1;
+        int i=0;
+        while(i<=end){
+            if(nums[i]==0){
+                swap(nums[i], nums[start]);
+                start++;
+            }
+            else if(nums[i]==2){
+                swap(nums[i], nums[end]);
+                end--;
+                continue;
+            }
+            i++;
+        }
+        
+    }
+};
